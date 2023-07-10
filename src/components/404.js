13ef0a270246c7bfd929e69c85e540c7d23d82cf
Vladislp <42935979+Vladislp@users.noreply.github.com>
2023-07-10 16:51:26 +0300

    3/5-Change-File-Of-Resume-In-Project-Change-Email-In-Contact-Me-Section
    Adding new resume + change email + working on error page

---
+++ src/components/404.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound404 = () => {
+      return(
+        <div>
+            <h1>404 Not found</h1>
+            <p>Sorry</p>
+        </div>
+    );
+};
+
+export default NotFound404;
---
+++ src/components/500.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound500 = () => {
+      return(
+        <div>
+            <h1>500 Not found</h1>
+            <p>Sorry</p>
+        </div>
+    );
+};
+
+export default NotFound500;
